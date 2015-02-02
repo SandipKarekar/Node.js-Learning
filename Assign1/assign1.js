a98c92fc4311372d45656f522ec4302dde9e6393
Shree74 <sandip.karekar@mindstix.com>
2015-02-02 19:03:09 +0530

    Added code to check if the source.json file is exists or not.

--- Assign1/assign1.js
+++ Assign1/assign1.js
@@ -5,78 +5,80 @@ var sourceFile = 'source/json/source.json';
 var destinFile = 'destination/txt/destination.txt';
 var xmlFile = 'destination/xml/final.xml';
 
-fs.readFile(sourceFile, 'utf8', function (err, data) {
+fs.exists(sourceFile, function (exists){
+  if(exists){
+    fs.readFile(sourceFile, 'utf8', function (err, data) {
 
-  console.log('\nSoucefile for JSON is: \'' + sourceFile + '\'');
+      console.log('\nSoucefile for JSON is: \'' + sourceFile + '\'');
   
-  if (err) {
+      if (err) {
 
-    console.log('\nError while reading the file' + err.message);
+        console.log('\nError while reading the file' + err.message);
     
-  } else {
+      } else {
 
-      try {
+          try {
 
-        // This try block for the handling the JSON which may 
-        // have the incorrect syntax which may cause the problem.
+            // This try block for the handling the JSON which may 
+            // have the incorrect syntax which may cause the problem.
         
-        var studentObject = JSON.parse(data);
+            var studentObject = JSON.parse(data);
 
-          var Validator = require('jsonschema').Validator;
-          var v = new Validator();
+              var Validator = require('jsonschema').Validator;
+              var v = new Validator();
 
-          // JSON schema has to create having two schemas as :-
-          // 'jsonOject' for Student object &
-          // 'jsonSchema' for actual JSON input.
+              // JSON schema has to create having two schemas as :-
+              // 'jsonOject' for Student object &
+              // 'jsonSchema' for actual JSON input.
 
-          var jsonObject = {
-              "id": "/StudentObject",
-              "type": "object",
-              "properties": {
-                 "id": {"type": "integer", "minimum": 1, "required": true},
-                "fName": {"type": "string", "required": true},
-                "lName": {"type": "string", "required": true},
-                "score": {"type": "integer", "minimum": 1, "required": true},
-              },
-            };
+              var jsonObject = {
+                  "id": "/StudentObject",
+                  "type": "object",
+                  "properties": {
+                    "id": {"type": "integer", "minimum": 1, "required": true},
+                    "fName": {"type": "string", "required": true},
+                    "lName": {"type": "string", "required": true},
+                    "score": {"type": "integer", "minimum": 1, "required": true},
+                  },
+                };
 
-          var jsonSchema = {
-            "id": "/JsonArrayObject",
-            "type": "object",
-            "properties": {
-              "students": {
-                "type": "array",
-                "items": {"$ref": "/StudentObject", "minimum": 1, "required": true}
-              }
-            }
-          }; 
+              var jsonSchema = {
+                "id": "/JsonArrayObject",
+                "type": "object",
+                "properties": {
+                  "students": {
+                    "type": "array",
+                    "items": {"$ref": "/StudentObject", "minimum": 1, "required": true}
+                  }
+               }
+              }; 
   
-          v.addSchema(jsonObject, '/StudentObject');
-          var result = v.validate(studentObject, jsonSchema);
+              v.addSchema(jsonObject, '/StudentObject');
+              var result = v.validate(studentObject, jsonSchema);
+  
+              if(result.errors.length){
+                // If JSON is not having expected format
+                // we have to tell user about that and can't
+                // go further. 
+                console.log("Input JSON is not having the required format " + result.errors);
+              } else {
 
-          if(result.errors.length){
-            // If JSON is not having expected format
-            // we have to tell user about that and can't
-            // go further. 
-            console.log("Input JSON is not having the required format " + result.errors);
-          } else {
-
-            fs.exists(destinFile, function (exists) {
+                fs.exists(destinFile, function (exists) {
           
-                  if(exists) {
+                    if(exists) {
 
-                    // Warning to user about the 
-                    //.txt file already file exists.
-                    console.log('\nFile named \'' + destinFile + '\' already exists\n\n\'Going to replace it\'');
+                      // Warning to user about the 
+                      //.txt file already file exists.
+                      console.log('\nFile named \'' + destinFile + '\' already exists\n\n\'Going to replace it\'');
 
                     }
-                    // Creating .txt file.
+                      // Creating .txt file.
 
-                    fs.writeFile(destinFile, 'First Name | Last Name | Score \n', function (err) {
+                      fs.writeFile(destinFile, 'First Name | Last Name | Score \n', function (err) {
 
                       if(err){
-                        // We cannot write to the file so that we are here.
-                        // we have to notify that about user.
+                      // We cannot write to the file so that we are here.
+                      // we have to notify that about user.
                         console.log('\nWe can not write to the file \"' + err + '\"');
                       } else {
                         /**
@@ -86,23 +88,23 @@ fs.readFile(sourceFile, 'utf8', function (err, data) {
                          * @return {difference of scores} this is used by sort function to compare those scores.
                          * Finally the sorted array of object is created.
                          */
-			                    studentObject.students = studentObject.students.sort(function (a, b) {
-			                      return b.score - a.score;
-			                    });
+                          studentObject.students = studentObject.students.sort(function (a, b) {
+                            return b.score - a.score;
+                          });
 
-			    
-			                    for (var i = 0; i < studentObject.students.length; i++) {
-			                      fs.appendFile(destinFile, studentObject.students[i].id + ' | ' 
-				                      + studentObject.students[i].fName + ' | '
-				                      + studentObject.students[i].lName + ' | '
-				                      + studentObject.students[i].score + '\n', function (err) {
-				                      if (err) { 
-				                        console.log('\nError while appending the file ' + err.message)
-				                      }
-			                      });
-			                    }
+          
+                          for (var i = 0; i < studentObject.students.length; i++) {
+                            fs.appendFile(destinFile, studentObject.students[i].id + ' | ' 
+                              + studentObject.students[i].fName + ' | '
+                              + studentObject.students[i].lName + ' | '
+                              + studentObject.students[i].score + '\n', function (err) {
+                              if (err) { 
+                                console.log('\nError while appending the file ' + err.message)
+                              }
+                            });
+                          }
 
-			                    console.log('\nData written to file: \'' + destinFile + '\'');
+                          console.log('\nData written to file: \'' + destinFile + '\'');
 
                           fs.exists(xmlFile, function (exists) {
       
@@ -145,7 +147,7 @@ fs.readFile(sourceFile, 'utf8', function (err, data) {
 
                         });
 
-			                  }
+                        }
                       });
                 });
 
@@ -156,3 +158,9 @@ fs.readFile(sourceFile, 'utf8', function (err, data) {
       }
     }
 });
+
+  } else {
+    console.log('\"Source file does not exists\"');
+  }
+});
+
